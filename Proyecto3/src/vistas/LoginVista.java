ff45a275f347092bfa91044f5181159fac23f089
Anama0956 <am.sandovalj1@uniandes.edu.co>
2025-12-06 21:25:11 -0500

    marketplace y login vista

--- Proyecto3/src/vistas/LoginVista.java
+++ Proyecto3/src/vistas/LoginVista.java
@@ -1,5 +1,93 @@
 package vistas;
 
-public class LoginVista {
+import java.awt.GridLayout;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import controladores.LoginController;
+import usuarios.Cliente;
+
+public class LoginVista extends JFrame{
+	
+	private JTextField txtUsuario;
+	private JPasswordField txtContrasena;
+	
+	private LoginController controller;
+	
+	public LoginVista(LoginController controller) {
+		this.controller = controller;
+		
+		setTitle("Login");
+		setSize(350, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		JLabel labUsuario = new JLabel("Usuario: ");
+		JTextField txtUsuario = new JTextField(15);
+		
+		JLabel labPass = new JLabel("Contraseña: ");
+		JPasswordField txtPass = new JPasswordField(15);
+		
+		JButton btnLogin = new JButton("Iniciar sesión");
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(new GridLayout(3,2,5,5));
+		
+		panel.add(labUsuario);
+		panel.add(txtUsuario);
+		panel.add(labPass);
+		panel.add(txtPass);
+		panel.add(new JLabel(""));
+		panel.add(btnLogin);
+		
+		add(panel);
+		
+		btnLogin.addActionListener(e -> {
+			
+			String user = txtUsuario.getText();
+			String pass = new String(txtPass.getPassword());
+			
+			Cliente cliente = controller.login(user, pass);
+			
+			if (cliente == null) {
+				JOptionPane.showMessageDialog(this,  "Usuario o contraseña incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			JOptionPane.showMessageDialog(this,  "Bienvenido, " + cliente.getNombreUsuario(), "Login exitoso", JOptionPane.INFORMATION_MESSAGE);
+			
+			ventanaPorRol(cliente);
+			
+		});
+		
+		setVisible(true);
+		
+	}
+
+	private void ventanaPorRol(Cliente cliente) {
+		
+		if (cliente instanceof usuarios.Administrador) {
+			new AdminVista();
+		}
+		
+		else if (cliente instanceof usuarios.Organizador) {
+			new OrganizadorVista();
+		}
+		
+		else if (cliente instanceof usuarios.ClienteNatural) {
+			new ClienteVista();
+		}
+		this.dispose();
+	}
+	
+
+	
+	
+	
 }
--- Proyecto3/src/vistas/MarketplaceVista.java
+++ Proyecto3/src/vistas/MarketplaceVista.java
@@ -1,5 +1,119 @@
 package vistas;
 
-public class MarketplaceVista {
+import java.awt.BorderLayout;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import controladores.MarketplaceController;
+import marketPlace.Oferta;
+import usuarios.ClienteNatural;
+
+public class MarketplaceVista extends JFrame{
+
+	private MarketplaceController controller;
+	private ClienteNatural usuario;
+	private JTable tabla;
+	
+	public MarketplaceVista(MarketplaceController controller, ClienteNatural usuario) {
+		this.controller = controller;
+		this.usuario = usuario;
+		
+		setTitle("MarketPlace");
+		setSize(700, 400);
+		setLocationRelativeTo(null);
+		setLayout(new BorderLayout());
+		
+		tabla = new JTable();
+		refrescarOfertas();
+		
+		add(new JScrollPane(tabla), BorderLayout.CENTER);
+		
+		JPanel panelBotones = new JPanel();
+		JButton btnComprar = new JButton("Comprar oferta");
+		JButton btnContraOferta = new JButton("Hacer contraoferta");
+		
+		panelBotones.add(btnComprar);
+		panelBotones.add(btnContraOferta);
+		
+		add(panelBotones, BorderLayout.SOUTH);
+		
+		btnComprar.addActionListener(e -> comprarOferta());
+		
+		btnComprar.addActionListener(e -> crearContraOferta());
+		
+		setVisible(true);
+		
+	}
+	
+	private void refrescarOfertas() {
+		
+		List<Oferta> ofertas = controller.obtenerOfertasDeCliente(usuario);
+		
+		DefaultTableModel modelo = new DefaultTableModel(
+				new Object[] {"ID", "Precio", "Vendedor", "Estado"}, 0);
+		
+		for (Oferta o: ofertas) {
+			modelo.addRow(new Object[] {
+					o.getId(),
+					o.getPrecioOferta(),
+					o.getVendedor().getNombreUsuario(),
+					o.getEstadoOf()
+			});
+		}
+		
+		tabla.setModel(modelo);
+	}
+	
+	private Oferta getOfertaSeleccionada() {
+		int fila = tabla.getSelectedRow();
+		if (fila == -1) {
+			JOptionPane.showMessageDialog(this,  "Selecciona una oferta");
+			return null;
+		}
+		
+		String idOferta = (String) tabla.getValueAt(fila,  0);
+		
+		for (Oferta o: controller.obtenerOfertasDeCliente(usuario)) {
+			if (o.getId().equals(idOferta)) {
+				return o;
+			}
+		}	
+		return null;
+	}               
+	
+	private void comprarOferta() {
+		Oferta oferta = getOfertaSeleccionada();
+		if (oferta == null) {
+			return;
+		}
+		
+		String metodo = JOptionPane.showInputDialog(null, "Método de pago: SALDO VIRTUAL o PASARELA");
+		
+		controller.comprarOferta(usuario, oferta, metodo);
+		
+		JOptionPane.showMessageDialog(this, "Compra realizada");
+		refrescarOfertas();
+	}
+	
+	private void crearContraOferta() {
+		Oferta oferta = getOfertaSeleccionada();
+		if (oferta == null) return;
+		
+		String precioStr = JOptionPane.showInputDialog("Nuevo precio: ");
+		double precio = Double.parseDouble(precioStr);
+		
+		String metodo = JOptionPane.showInputDialog("Método de pago: ");
+		
+		controller.crearContraOferta(usuario, oferta, precio, metodo);
+		
+		JOptionPane.showMessageDialog(this,  "Contraoferta enviada");
+	}
+	
 }
